32093639fb3310b12bd8c65fd2a3418188d0c433
Fabrizio Candi <fabriziocandi@MacBook-Air-di-Fabrizio.local>
2026-04-21 15:24:53 +0800

    fix whatsapp link

--- app/page.tsx
+++ app/page.tsx
@@ -421,22 +421,10 @@ export default function Home() {
               >
                 Contact
               </a>
-              <a
-                href="#"
-                onClick={(e) => {
-                  e.preventDefault();
-
-                  if (typeof window !== "undefined" && window.gtag) {
-                    window.gtag("event", "click_whatsapp", {
-                      event_category: "engagement",
-                      event_label: "homepage_whatsapp",
-                    });
-                  }
-
-                  setTimeout(() => {
-                    window.open("https://wa.me/6282146574879", "_blank");
-                  }, 300);
-                }}
+              <<a
+                href="https://wa.me/6282146574879"
+                target="_blank"
+                rel="noopener noreferrer"
               >
                 +62 821-4657-4879
               </a>
